eb3c0e49b3defe1f4a62626573fbd1773d9b6a0d
miyacchi85 <miyahira@skyarch.net>
2024-03-29 09:34:39 +0900

    clear_station2

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PracticeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('practice', function() {
-    return response('practice');
-});
-
-Route::get('practice2', function() {
-    $test = 'practice2';
-return response($test);
-});
-
-Route::get('practice3', function() {
-    $test2 = 'test';
-return response($test2);
-});
+// Route::get('URL', [Controllerの名前::class, 'Controller内のfunction名']);
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);
